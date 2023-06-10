3f9a37f6e6a28ba66199f33c57055c064395b1af
Jorge Garcia <jegcelorio@yahoo.com>
2023-06-10 08:40:11 -0500

    Implemented kidsWithCandies

--- array-string/kidsGreatestCandies.js
+++ array-string/kidsGreatestCandies.js
@@ -1,3 +1,24 @@
 var kidsWithCandies = function(candies, extraCandies) {
-    
+    let results = [];
+    let max = Math.max(...candies);
+
+    for(let i = 0; i < candies.length; i++) {
+        
+        if (candies[i] + extraCandies >= max) {
+            results.push(true);
+        }
+        else
+        {
+            results.push(false);
+        }
+        
+    }
+
+    return results;
 }
+
+var candies = [2,3,5,1,3];
+var extraCandies = 3;
+var maxCandies = kidsWithCandies(candies, extraCandies);
+
+console.log(maxCandies);
