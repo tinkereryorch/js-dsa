b0830fa675a6de2a594359a91d93894aec2ecee7
Jorge Garcia <jegcelorio@yahoo.com>
2023-06-09 23:19:46 -0500

    Implemented Euclid's algorithm

--- array-string/gcdOfStrings.js
+++ array-string/gcdOfStrings.js
@@ -1,11 +1,22 @@
-var gcdOfStrings = function(str1, str2) {
-    
-    if (str1 + str2 !== str2 + str1) {
-        return '';
+var gcdOfStrings = function (str1, str2) {
+    let x = '';
+
+    if (str1.concat(str2) != str2.concat(str1)) return x;
+
+    let gcd = (x, y) => {
+        if (y === 0) {
+            return x;
+        }
+
+        return gcd(y, x % y);
     }
 
-    
+    let particle = gcd(str1.length, str2.length);
+
+    return str1.slice(0, particle);
 }
 
-var result = gcdOfStrings('AB' ,'A');
-console.log(result);
+var result = gcdOfStrings('ABABAB', 'ABAB');
+console.log(result); // 'AB'
+
+
