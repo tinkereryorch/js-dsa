78894ed32a56be64155f789613e8c74aac25ae9b
Jorge Garcia <jegcelorio@yahoo.com>
2023-06-09 21:40:13 -0500

    Added gcdOfStrings exercise

---
+++ array-string/gcdOfStrings.js
@@ -0,0 +1,11 @@
+var gcdOfStrings = function(str1, str2) {
+    
+    if (str1 + str2 !== str2 + str1) {
+        return '';
+    }
+
+    
+}
+
+var result = gcdOfStrings('AB' ,'A');
+console.log(result);
