8dc2b1eb6c105fc30b41447625766bdec7652a60
Jorge Garcia <jegcelorio@yahoo.com>
2023-06-13 00:29:32 -0500

    Implemented productExceptSelf

--- array-string/productExceptSelf.js
+++ array-string/productExceptSelf.js
@@ -1,15 +1,22 @@
+// The underlying concept is the prefixSum 
 var productExceptSelf = function(nums) {
     const result = [];
-
     let prefix = 1;
+    let suffix = 1;
 
     for(let i = 0; i < nums.length; i++) {
         result[i] = prefix;
         prefix *= nums[i];
     }
 
-    console.log(result, prefix);
+    for(let i = nums.length -1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return result;
 }
 
 let nums = [1,2,3,4];
-productExceptSelf(nums);
+var product = productExceptSelf(nums);
+console.log(product);
