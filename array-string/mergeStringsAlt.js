7227d56c6db2d2715e416ba609a723f9d27aa8fd
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-21 19:59:16 -0500

    Added description

--- array-string/mergeStringsAlt.js
+++ array-string/mergeStringsAlt.js
@@ -1,1 +1,6 @@
 // Merge Strings Alternately - LeetCode 1768
+
+/* You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. 
+If a string is longer than the other, append the additional letters onto the end of the merged string.
+*/
+
